47d3e31df85484c65d9e75b13bfe0e1000222aa9
Boyd Halfman <78419518+STR3ETS@users.noreply.github.com>
2025-09-29 15:55:15 +0200

    Laatste fixes

--- resources/views/account/set-password.blade.php
+++ resources/views/account/set-password.blade.php
@@ -12,7 +12,7 @@
       <input type="password" name="password" class="w-full rounded-xl border-[#ededed] hover:border-[#c7c7c7] transition duration-300
                         p-3
                         focus:outline-none focus:ring-0 focus-visible:outline-none focus-visible:ring-0
-                        focus:border-[#c8ab7a] text-sm" required>
+                        focus:border-[#c8ab7a] text-[16px] md:text-sm" required>
       @error('password') <p class="text-sm text-red-600 mt-1">{{ $message }}</p> @enderror
     </div>
     <div>
@@ -20,7 +20,7 @@
       <input type="password" name="password_confirmation" class="w-full rounded-xl border-[#ededed] hover:border-[#c7c7c7] transition duration-300
                         p-3
                         focus:outline-none focus:ring-0 focus-visible:outline-none focus-visible:ring-0
-                        focus:border-[#c8ab7a] text-sm" required>
+                        focus:border-[#c8ab7a] text-[16px] md:text-sm" required>
     </div>
     <button
     class="px-6 py-3 bg-[#c8ab7a] hover:bg-[#a38b62] transition duration-300 text-white font-medium text-sm rounded mt-4">
